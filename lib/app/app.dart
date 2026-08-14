036fbcc458898559b4af09b250f55ef756635743
Aurelianoluiz <151521181+Aurelianoluiz@users.noreply.github.com>
2026-08-13 22:14:53 -0300

    feat: enable responsive app shell

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import '../core/brand.dart';
 import '../core/theme.dart';
-import '../features/dashboard/dashboard_page.dart';
+import 'app_shell.dart';
 
 class LiveStudioApp extends StatelessWidget {
   const LiveStudioApp({super.key});
+
   @override
   Widget build(BuildContext context) => MaterialApp(
         title: Brand.name,
         debugShowCheckedModeBanner: false,
         theme: AppTheme.light(),
-        home: const DashboardPage(),
+        home: const AppShell(),
       );
 }
