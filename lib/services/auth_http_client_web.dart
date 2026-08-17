54dd08850b6a04c834a0483e04a431d9b350e1ff
Aurelianoluiz <151521181+Aurelianoluiz@users.noreply.github.com>
2026-08-16 22:19:16 -0300

    fix: configure browser auth credentials compatibly

--- lib/services/auth_http_client_web.dart
+++ lib/services/auth_http_client_web.dart
@@ -1,4 +1,8 @@
 import 'package:http/browser_client.dart';
 import 'package:http/http.dart' as http;
 
-http.Client createAuthHttpClient() => BrowserClient(withCredentials: true);
+http.Client createAuthHttpClient() {
+  final client = BrowserClient();
+  client.withCredentials = true;
+  return client;
+}
