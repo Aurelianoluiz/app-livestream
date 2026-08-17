7ec73967fa50e286b805df74fbf56a94ca96d335
Aurelianoluiz <151521181+Aurelianoluiz@users.noreply.github.com>
2026-08-16 22:12:19 -0300

    fix: add cross-platform auth client factory

---
+++ lib/services/auth_http_client.dart
@@ -0,0 +1,6 @@
+import 'package:http/http.dart' as http;
+
+import 'auth_http_client_io.dart'
+    if (dart.library.html) 'auth_http_client_web.dart' as platform;
+
+http.Client createAuthHttpClient() => platform.createAuthHttpClient();
